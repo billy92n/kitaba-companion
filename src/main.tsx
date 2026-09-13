c261c5b01c4775bb121f8b599ba1af84474d01df
billy92n <sifodil.osteo@gmail.com>
2026-09-13 19:40:20 +0200

    Mount encyclopedia integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { VisualReferenceDock } from "./components/VisualReferenceDock";
+import { EncyclopediaDock } from "./components/EncyclopediaDock";
 import "./player-knowledge-hardening.css";
 import "./mvp-theme-hardening.css";
 import "./image-fit-hardening.css";
+import "./encyclopedia.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode><><App /><VisualReferenceDock /></></StrictMode>
+  <StrictMode><><App /><VisualReferenceDock /><EncyclopediaDock /></></StrictMode>
 );
